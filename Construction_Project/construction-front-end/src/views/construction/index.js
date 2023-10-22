99316bbafb40d693f4446110d47bf64409fab514
Indresh535 <indreshindresh535@gmail.com>
2023-10-22 17:30:57 +0530

    Updated UI correectly

--- Construction_Project/construction-front-end/src/views/construction/index.js
+++ Construction_Project/construction-front-end/src/views/construction/index.js
@@ -15,12 +15,11 @@ const Construction = () => {
     const handleSupplierChange = (event) => {
       const selectSupplier = event.target.value;
       setSelectedSupplier(selectSupplier);
-      //console.log(' selectedSupplier', selectedSupplier);
-  
+      //console.log(' selectedSupplier', selectedSupplier);  
+      setLoading(true)
       // Send a POST request with the selected supplier
       axios.post('/getAllPoNumberOnSupplier', { supplier: selectSupplier })
         .then((res) => {
-          setLoading(true)
           setPoNumbers(res.data);
           setLoading(false)
          // console.log("getAllPoNumberOnSupplier", res.data)
@@ -35,12 +34,10 @@ const Construction = () => {
       const selectPoNumber = event.target.value;
       setLoading(true)
       setSelectedPoNumber(selectPoNumber);
-      setLoading(false)
      // console.log("selectedPoNumber", selectedPoNumber)
       // Send a POST request with the selected supplier
       axios.post('/getAllDataOnPoNumber', { poNumber: selectPoNumber })
         .then((res) => {
-            setLoading(true)
           setData(res.data);
           setLoading(false)
          // console.log("getAllDataOnPoNumber", res.data)
@@ -52,9 +49,9 @@ const Construction = () => {
   
     useEffect(() => {
       // Fetch all suppliers
+            setLoading(true)
       axios.get('/GetAllSupplier')
         .then((res) => {
-            setLoading(true)
           setSuppliers(res.data);
           setLoading(false)
         })
@@ -110,17 +107,21 @@ const Construction = () => {
                     <FormHelperText>Select Supplier To Display PO Number</FormHelperText>
                 </FormControl>
                 </Grid>
-                <Grid item sm={12} md={6} lg={4} className='border-t-2'>
-                    {data.map((d, index) => (
-                        <Card sm={12} md={6} lg={4} key={index} className='m-2 p-4 border rounded-lg shadow-md hover:bg-slate-100'>
-                            <p className="text-lg font-semibold my-4 border-b-2 p-2">PO_Number: {d.PO_Number}</p>
-                            <p className="text-lg font-semibold my-4 border-b-2 p-2">Order_Value: {d.Order_Value}</p>
-                            <p className="text-lg font-semibold my-4 border-b-2 p-2">Amount_Invoiced: {d.Amount_Invoiced}</p>
-                            <p className="text-lg font-semibold my-4 border-b-2 p-2">Description: {d.Description}</p>
-                        </Card>
-                    ))}
-                </Grid>
-        </Grid>
+                <Grid item xs={12} className='border-t-2 flex flex-wrap'>
+                        <Grid container spacing={2}>
+                        {data.map((d, index) => (
+                            <Grid item xs={4} md={6} sm={12} key={index}>
+                                <Card className='m-2 p-4 border rounded-lg shadow-md hover:bg-slate-100 flex flex-col items-start'>
+                                    <p className="text-lg font-semibold my-4 border-b-2 p-2">PO_Number: {d.PO_Number}</p>
+                                    <p className="text-lg font-semibold my-4 border-b-2 p-2">Order_Value: {d.Order_Value}</p>
+                                    <p className="text-lg font-semibold my-4 border-b-2 p-2">Amount_Invoiced: {d.Amount_Invoiced}</p>
+                                    <p className="text-lg font-semibold my-4 border-b-2 p-2">Description: {d.Description}</p>
+                                </Card>
+                            </Grid>
+                        ))}
+                    </Grid>
+                </Grid>         
+            </Grid>
         </Box>    
         {loading && (
             <div className='flex justify-center items-center'>
